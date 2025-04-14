4f1b64e200ed4d95d5461a74f50315b776a9580e
Sasmuka06 <samuelssf881@gmail.com>
2025-04-14 18:18:03 -0300

    Adcionei o model no login e criei uma classe chamada model

--- PW3/Controllers/LoginController.cs
+++ PW3/Controllers/LoginController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PW3.Models;
 
 namespace PW3.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Logar()
+        [HttpGet]
+        public IActionResult Logar(LoginModel dados)
         {
+            LoginModel model = new LoginModel();
+            model.Login = "sasmuka@gmail.com";
+            model.Senha = "Bomdia";
             return View();
         }
     }
